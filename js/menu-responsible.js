56b10753ccc4eb7bf1ec356a22d53b7e9e87e6e9
Oscar Jesus <jesus_sa_es@hotmail.com>
2021-02-11 19:52:58 -0500

    web

---
+++ js/menu-responsible.js
@@ -0,0 +1,13 @@
+jQuery('document').ready(function($){
+    var menuBtn = $('.menu-icon');
+    var menu = $('nav ul');
+
+    menuBtn.click(function(){
+        if(menu.hasClass('show')){
+            menu.removeClass('show');
+        } else {
+            menu.addClass('show');
+        }
+    });
+    
+});
